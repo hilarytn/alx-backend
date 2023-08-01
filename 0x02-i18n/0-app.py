fbd7315cf4dc7490ea61fe0fd295e0dc9852573b
Hilary Titus Naor <hilarytn@gmail.com>
2023-08-01 14:12:26 +0000

    Set-up a basic flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+"""A basic Flask app to show i18n/L10n"""
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    """A simple view for the index file"""
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
