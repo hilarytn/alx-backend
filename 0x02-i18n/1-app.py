1ef022308bf0d06d0b341a699547a5cf2800cf0c
Hilary Titus Naor <hilarytn@gmail.com>
2023-08-04 14:01:54 +0000

    Configure Flask-Babel

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+from flask import Flask, render_template
+from flask_babel import Babel
+"""This module is a basic Flask
+   app to show i18n/L10n and also
+   install the Flask-Babel library
+   and configure it
+"""
+
+
+app = Flask(__name__)
+
+babel = Babel(app)
+
+
+class Config(object):
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def index():
+    """A simple view for the index file"""
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
