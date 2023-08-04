683fd0c0a078553e11c1b4f267a64bdb68d94dcd
Hilary Titus Naor <hilarytn@gmail.com>
2023-08-04 14:22:53 +0000

    Fix documentation

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
-from flask import Flask, render_template
-from flask_babel import Babel
 """This module is a basic Flask
    app to show i18n/L10n and also
    install the Flask-Babel library
    and configure it
 """
+from flask import Flask, render_template
+from flask_babel import Babel
 
 
 app = Flask(__name__)
-
 babel = Babel(app)
 
 
